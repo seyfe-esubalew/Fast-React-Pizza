d24e8a138e74f358765da871d6200c3d6b23be50
eeyfesubalew <esubalew.seife@gmail.com>
2023-10-02 22:25:20 +0300

    adding loading feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      { path: "order/new", element: <CreateOrder /> },
+      { path: "order/?orderID", element: <Order /> },
+    ],
   },
-  {
-    path: "/menu",
-    element: <Menu />,
-  },
-  {
-    path: "/cart",
-    element: <Cart />,
-  },
-  { path: "order/new", element: <CreateOrder /> },
-  { path: "order/?orderID", element: <Order /> },
 ]);
 function App() {
   return <RouterProvider router={router} />;
---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div>
+      <Link to="/">Fast React Pizza Co.</Link>
+      <p>Seyfe Esubalew</p>
+    </div>
+  );
+}
+
+export default Header;
